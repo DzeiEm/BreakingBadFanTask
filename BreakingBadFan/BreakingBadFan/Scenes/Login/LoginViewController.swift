80fd9d0dd52b020b389903820bc0d8dde5006904
DzeiEm <julija.melesina@gmail.com>
2022-08-07 18:30:39 +0300

    TextFieldDidBeginEditing func added

--- BreakingBadFan/BreakingBadFan/Scenes/Login/LoginViewController.swift
+++ BreakingBadFan/BreakingBadFan/Scenes/Login/LoginViewController.swift
@@ -9,7 +9,7 @@ enum SegmentTitle: String {
 
 class LoginViewController: UIViewController {
     
-
+    
     //OUTLETS
     @IBOutlet weak var registrationTypeSegmentController: UISegmentedControl!
     @IBOutlet weak var usernameTextfield: UITextField!
@@ -17,32 +17,36 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var confirmPasswordTextfield: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var buttonLabel: UIButton!
-    
-    let ui = LoginUI()
+    var username: String = ""
+    var password: String = ""
+    var confirmPassword: String = ""
    
+    let ui = LoginUI()
     
     //ACTIONS
     @IBAction func onSegmentControllerTypeChanged(_ sender: UISegmentedControl) {
-       
+        
         if registrationTypeSegmentController.selectedSegmentIndex == 0 {
-            usernameTextfield.isHidden = false
-            passwordTextfield.isHidden = false
-            confirmPasswordTextfield.isHidden = false
             errorLabel.isHidden = true
+            hideTextfield(textfield: usernameTextfield, hide: false)
+            hideTextfield(textfield: passwordTextfield, hide: false)
+            hideTextfield(textfield: confirmPasswordTextfield, hide: false)
             buttonLabel.titleLabel?.text = SegmentTitle.Register.rawValue
+            buttonLabel.isEnabled =  false
         } else {
-            usernameTextfield.isHidden = false
-            passwordTextfield.isHidden = false
-            confirmPasswordTextfield.isHidden = true
             errorLabel.isHidden = true
+            hideTextfield(textfield: usernameTextfield, hide: false)
+            hideTextfield(textfield: passwordTextfield, hide: false)
+            hideTextfield(textfield: confirmPasswordTextfield, hide: true)
             buttonLabel.titleLabel?.text = SegmentTitle.Login.rawValue
+            buttonLabel.isEnabled =  false
         }
         
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         do {
-            try checkIfNoEmpty()
+            try checkIfTextfieldsNotEmpty()
         } catch {
             //TODO
         }
@@ -50,19 +54,49 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      ui.roundCorners(of: buttonLabel, by: 20)
+        ui.roundCorners(of: buttonLabel, by: 20)
         errorLabel.isHidden = true
+        buttonLabel.isEnabled = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.textFieldDidBeginEditing(usernameTextfield)
+        
     }
     
     
-    func checkIfNoEmpty() throws {
-        guard let username = usernameTextfield.text,
-              let password = passwordTextfield.text,
-              let confirmPassword  = confirmPasswordTextfield.text,
-              username.isEmpty,
-              password.isEmpty,
-              confirmPassword.isEmpty else  {
+    func checkIfTextfieldsNotEmpty() throws -> Bool {
+        if username == usernameTextfield.text,
+           password == passwordTextfield.text,
+           confirmPassword == confirmPasswordTextfield.text,
+           username.isEmpty,
+           password.isEmpty,
+           confirmPassword.isEmpty {
             throw LoginError.emptyField
+        }
+        return true
+    }
+    
+    private func hideTextfield( textfield: UITextField?, hide: Bool) {
+        textfield?.isHidden = hide
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textfield: UITextField) {
+        if textfield.text == self.username {
+            buttonLabel.isEnabled = true
+            print("username textfield tapped")
+        }
+        if textfield.text == self.password {
+            buttonLabel.isEnabled = true
+            print("passwod textfield tapped")
+        }
+        if textfield.text == self.confirmPassword {
+            buttonLabel.isEnabled = true
+            print("confirm password textfield tapped")
         }
     }
     
