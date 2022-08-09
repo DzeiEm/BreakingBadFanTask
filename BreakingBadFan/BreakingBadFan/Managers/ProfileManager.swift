208d43e69be76047fc674cf7f4545fac758361d4
DzeiEm <julija.melesina@gmail.com>
2022-08-09 20:20:32 +0300

    Secure password validation implemented

--- BreakingBadFan/BreakingBadFan/Managers/ProfileManager.swift
+++ BreakingBadFan/BreakingBadFan/Managers/ProfileManager.swift
@@ -1,6 +1,7 @@
 
 
 import Foundation
+import CoreVideo
 
 class ProfileManager {
     
@@ -9,9 +10,12 @@ class ProfileManager {
         case userNotFound
         case accountAlreadyExist
         case passwordNOtSecure
+        case emptyFields
         
         var errorMessage: String {
             switch self {
+            case.emptyFields:
+                return "Tiextfield caanot be empy"
             case .incorrectCredentials:
                 return "Username or password incorrect"
             case .userNotFound:
@@ -25,6 +29,32 @@ class ProfileManager {
         
     }
     
+    enum SecurePasswordError: Error {
+        case containsNumbers
+        case containsLowerCase
+        case contaionUpperCase
+        case charactersCount
+        
+        var errroMessage: String {
+            switch self {
+            case .containsNumbers:
+                return "Pasword should contains numbers"
+            case .containsLowerCase:
+                return "Pasword should contains lower cases"
+            case .contaionUpperCase:
+                return "Pasword should contains upper cases"
+            case .charactersCount:
+                return "Passowrd is too weak, should be at least 8 characters  "
+            }
+        }
+    }
+    
+    
+    
+    //    static var profile: Profile {
+    //
+    //    }
+    
     static func login(username: String, password: String) {
         
     }
@@ -32,5 +62,60 @@ class ProfileManager {
     static func register(username: String, password: String) {
         
     }
+}
+
+
+extension ProfileManager {
     
+    private func checkCredentialsAreNotEmpty(username: String?, password: String?) throws -> Bool {
+        guard let username = username,
+              let password = password,
+              !username.isEmpty,
+              !password.isEmpty
+        else {
+            throw ProfileManagerError.emptyFields
+        }
+        return true
+    }
+    
+    private func chaeckPasswordIsSecure(password: String) throws -> Bool {
+        let password = password
+        
+        guard password.containsNumbers else {
+            throw SecurePasswordError.containsNumbers
+        }
+        guard password.containsLowercase else {
+            throw SecurePasswordError.containsLowerCase
+        }
+        guard password.containsUppercase else {
+            throw SecurePasswordError.contaionUpperCase
+        }
+        guard password.containsEightCharacters else {
+            throw SecurePasswordError.charactersCount
+        }
+        return true
+    }
+    
+    
+    
+    private func passswordMatch() -> Bool {
+        
+        return true
+    }
+}
+
+extension String {
+    
+    var containsLowercase: Bool {
+        return self == self.lowercased()
+    }
+    var containsUppercase: Bool {
+        return self == self.uppercased()
+    }
+    var containsNumbers: Bool {
+        return (self.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) != nil)
+    }
+    var containsEightCharacters: Bool {
+        return self.count <= 8
+    }
 }
